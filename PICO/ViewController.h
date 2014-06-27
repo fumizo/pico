6e3b3c5db8fdba8e22ed6077cf024a3d6f2564aa
FUMIZO <fumi811bbc@gmail.com>
2014-06-27 19:37:49 +0900

    firs commit

--- PICO/ViewController.h
+++ PICO/ViewController.h
@@ -8,6 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    UIImageView *maru;
+    
+    UIImage *maruArray[4];
+}
+
+
 
 @end
--- PICO/ViewController.m
+++ PICO/ViewController.m
@@ -18,6 +18,53 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //maru =[[UIImageView alloc] initWithFrame:CGRectMake (80,284,50,50)];
+//    maruArray[1] =[[UIImageView alloc] initWithFrame:CGRectMake (60,234,50,50)];
+//    maruArray[2] =[[UIImageView alloc] initWithFrame:CGRectMake (80,234,50,50)];
+//    maruArray[3] =[[UIImageView alloc] initWithFrame:CGRectMake (60,284,50,50)];
+//    maru.image = [UIImage imageNamed:@"maru.png"];
+//    [self.view addSubview:maru];
+    
+    maruArray[0] = [UIImage imageNamed:@"maru.png"];
+    maruArray[1] = [UIImage imageNamed:@"maru.png"];
+    maruArray[2] = [UIImage imageNamed:@"maru.png"];
+    maruArray[3] = [UIImage imageNamed:@"maru.png"];
+    //[maru setImage:maruArray[0]];
+    
+    //maru.frame = CGRectMake(30, 30, 50, 50);
+    //[self.view addSubview:maru];
+    
+    
+    for (int i = 0; i < 4; i++) {
+        maru = [[UIImageView alloc] init];
+        maru.image = maruArray[i];
+        if (i == 0) {
+            maru.frame = CGRectMake(110, 190, 50, 50);
+            [self.view addSubview:maru];
+        }else if (i == 1) {
+            maru.frame = CGRectMake(160, 190, 50, 50);
+            [self.view addSubview:maru];
+        }else if (i == 2) {
+            maru.frame = CGRectMake(110, 240, 50, 50);
+            [self.view addSubview:maru];
+        }else if (i == 3) {
+            maru.frame = CGRectMake(160, 240, 50, 50);
+            [self.view addSubview:maru];
+        }
+
+    }
+    
+    /* 時計回りに45度回転 */
+    CGRect rect = [[UIScreen mainScreen] applicationFrame];
+    NSLog(@"%@", NSStringFromCGRect(rect));
+    float x = rect.size.width/2;
+    float y = rect.size.height/2;
+    maru.center = CGPointMake(x, y);
+    CGAffineTransform transform = CGAffineTransformMakeRotation(M_PI/4);
+    [maru setTransform:transform];
+
+    
 }
 
 - (void)didReceiveMemoryWarning
